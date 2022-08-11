bf499fb0bebba9e923e7a71825f82dc8dff01c33
equipe-de-trabalho <projetotrilhas07@gmail.com>
2022-08-11 20:11:20 -0300

    criado o repositorio

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace App_Conta_Bancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
